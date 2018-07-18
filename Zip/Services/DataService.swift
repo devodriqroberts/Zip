d908f102463a5d0d976215242a696fb6d01a396b
DevoRoberts <devorob26@gmail.com>
2018-07-17 23:04:18 -0400

    Added firebase data base references.

---
+++ Zip/Services/DataService.swift
@@ -0,0 +1,67 @@
+//
+//  DataService.swift
+//  Zip
+//
+//  Created by Devodriq Roberts on 7/17/18.
+//  Copyright © 2018 Devodriq Roberts. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+
+let DB_BASE = Database.database().reference()
+
+class DataService {
+    
+    static let instance = DataService()
+    
+    private var _Ref_BASE = DB_BASE
+    
+    // Create users directory withing FB DB
+    private var _REF_USERS = DB_BASE.child("users")
+    
+    // Create drivers directory withing FB DB
+    private var _REF_DRIVERS = DB_BASE.child("drivers")
+    
+    // Create trips directory withing FB DB
+    private var _REF_TRIPS = DB_BASE.child("trips")
+    
+    
+    //Data encapsulation
+    var REF_BASE: DatabaseReference {
+        return _Ref_BASE
+    }
+    
+    var REF_USERS: DatabaseReference {
+        return _REF_USERS
+    }
+    
+    var REF_DRIVERS: DatabaseReference {
+        return _REF_DRIVERS
+    }
+    
+    var REF_TRIPS: DatabaseReference {
+        return _REF_TRIPS
+    }
+    
+    func createFirebaseDBUser(uid:String, userData: [String:Any], isDriver: Bool) {
+        
+        //Check if user is driver and updates user data for appropriate user
+        if isDriver {
+            REF_DRIVERS.child(uid).updateChildValues(userData)
+        } else {
+            REF_USERS.child(uid).updateChildValues(userData)
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
